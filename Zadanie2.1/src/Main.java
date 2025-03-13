890a6e5fc5e3ef393733f802ac19473dbc74aca4
Kamil Trzykowski <s33609@pjwstk.edu.pl>
2025-03-13 13:28:58 +0100

    Zadanie

--- Zadanie2.1/src/Main.java
+++ Zadanie2.1/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int cena = 40;
+        double znizka = 1;
+        System.out.println("Podaj wiek: ");
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        System.out.println("Podaj miasto: ");
+        String miasto = sc.next();
+        System.out.println("Podaj dzien tyg: ");
+        String dzien = sc.next();
+        if (w < 10) znizka = 0;
+        if (w >= 10 && w <=18) znizka -= 0.5;
+        if (miasto.equals("warszawa")) znizka -= 0.1;
+        if (dzien.equals("czwartek")) znizka = 0;
+        if (znizka < 0) znizka = 0;
+
+        System.out.println(cena*znizka);
     }
 }

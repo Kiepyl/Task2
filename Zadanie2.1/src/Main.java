2afefc7265bf0e44c7f549d3c769d1338f5fdf90
Kamil Trzykowski <s33609@pjwstk.edu.pl>
2025-03-13 13:39:51 +0100

    Final version

--- Zadanie2.1/src/Main.java
+++ Zadanie2.1/src/Main.java
@@ -16,6 +16,7 @@ public class Main {
         if (miasto.equals("warszawa")||miasto.equals("Warszawa")) znizka -= 0.1;
         if (dzien.equals("czwartek")||dzien.equals("Czwartek")) znizka = 0;
         if (znizka < 0) znizka = 0;
-        System.out.println(cena*znizka);
+        System.out.println("Wiek: " + w + " Miasto: " + miasto + " Dzien tyg: " + dzien);
+        System.out.println("Cena biletu po znizkach: " + cena*znizka);
     }
 }
